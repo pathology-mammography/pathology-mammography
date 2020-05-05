d92c571691dd728ecac1889b07520c680c1f7443
Anonymous <>
2020-05-05 17:19:11 +0900

    removed unused code.

--- code/predict.py
+++ code/predict.py
@@ -111,10 +111,6 @@ def create_model():
   input_shape = (image_size, image_size, 3)
   base_model = VGG16(weights= 'imagenet', include_top=False, input_shape=input_shape)
   #最終層の設定
-  x = base_model.output
-  x = GlobalAveragePooling2D()(x)
-  #x = Dense(1024, activation='relu')(x)
-  predictions = Dense(num_classes, activation = 'softmax')(x)
   m = Sequential()
   m.add(Flatten(input_shape=base_model.output_shape[1:]))
   m.add(Dense(256, activation='relu', kernel_initializer='he_normal'))
